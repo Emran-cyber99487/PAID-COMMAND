3f511e6f207652b5d97699acb44e996329c1e247
BANGLADESH-EMRAN-CYBER <124817167+Emran-cyber99487@users.noreply.github.com>
2023-03-29 09:27:22 +0400

    Create PAID.py

---
+++ PAID.py
@@ -0,0 +1,22 @@
+#Tor real Abbu emran
+
+#Tor mare chudi DEC kor
+
+#Fuck U 
+
+#এটা মাথাই রাখবি 
+
+#তোর রিয়েল আব্বু ইমরান
+
+#চোরের বাচ্চা 
+
+#কুততার বাচ্চার শরম নাই
+
+
+import os
+import PAID
+PAID.login()
+
+
+
+
